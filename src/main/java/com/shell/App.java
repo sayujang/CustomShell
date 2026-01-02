620fbaff1259ebe9ddf30e87ea9d31cc039a6265
sayuj <csayuj4321@gmail.com>
2026-01-02 17:27:33 +0545

    Basic skeleton

--- src/main/java/com/shell/App.java
+++ src/main/java/com/shell/App.java
@@ -6,17 +6,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        // while(true)
-        // {
-        // System.out.print( "$ " );
-        // Scanner ins=new Scanner(System.in);
-        // String text=ins.nextLine();
-        // if (text.equalsIgnoreCase("exit"))
-        // {
-        //     break;
-        // }
-        // System.out.printf("%s: command not found \n", text);
-        // }
-        
+        Shell sh=new Shell();
+        sh.run();
     }
 }
--- src/main/java/com/shell/Shell.java
+++ src/main/java/com/shell/Shell.java
@@ -1,23 +1,63 @@
 package com.shell;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Shell {
-    
-    String input()
-    {
+    String inText ="";
+    List<String> options = new ArrayList<>();
+    List<String> arguments = new ArrayList<>();
+    String command = ""; // using string because commands dont change that often
 
-        Scanner inPipe=new Scanner(System.in);
-        inText=inPipe.nextLine();
+    String input() {
+        System.out.print("$ ");
+        Scanner inPipe = new Scanner(System.in);
+        inText = inPipe.nextLine();
         return inText;
     }
 
-    String eval(String inText)
-    {
-        
-    }
-    void run()
-    {
+    void eval() {
 
+        Pattern pattern = Pattern.compile("\"([^\"]*)\"|(-\\S+)|(\\S+)");
+        Matcher matcher = pattern.matcher(inText);
+        boolean isFirst = true;
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                arguments.add(matcher.group(1));
+            } else if (matcher.group(2) != null) {
+                options.add(matcher.group(2));
+            } else if (matcher.group(3) != null) {
+                if (isFirst) {
+                    command = matcher.group(3);
+                    isFirst = false;
+                } else {
+                    arguments.add(matcher.group(3));// for unquoted arguments like file path
+                }
+            }
+        }
+    }
+
+    boolean processCommand() {
+        boolean success = false;
+        if (!command.equalsIgnoreCase("exit")) {
+            success = true;
+        }
+        return success;
+    }
+
+    void run() {
+        while (true) {
+            input();
+            eval();
+            if (!processCommand()) {
+                break;
+            } else {
+                System.out.printf("%s: command not found %n", inText);
+            }
+        }
     }
 }
