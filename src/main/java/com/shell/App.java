4b9ccedefca2fe5dccf82426715143bab21cac51
sayuj <csayuj4321@gmail.com>
2026-01-02 08:39:49 +0545

    first commit

---
+++ src/main/java/com/shell/App.java
@@ -0,0 +1,22 @@
+package com.shell;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+        // while(true)
+        // {
+        // System.out.print( "$ " );
+        // Scanner ins=new Scanner(System.in);
+        // String text=ins.nextLine();
+        // if (text.equalsIgnoreCase("exit"))
+        // {
+        //     break;
+        // }
+        // System.out.printf("%s: command not found \n", text);
+        // }
+        
+    }
+}
---
+++ src/main/java/com/shell/Shell.java
@@ -0,0 +1,23 @@
+package com.shell;
+
+import java.util.Scanner;
+
+public class Shell {
+    
+    String input()
+    {
+
+        Scanner inPipe=new Scanner(System.in);
+        inText=inPipe.nextLine();
+        return inText;
+    }
+
+    String eval(String inText)
+    {
+        
+    }
+    void run()
+    {
+
+    }
+}
